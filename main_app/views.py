7ca2837c55166c80c5ee2c422c905a484b7871fd
Student Workspace <student@Candices-MacBook-Air.local>
2019-03-15 18:56:55 -0500

    modified models,templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Event
 
 # Create your views here.
 
 def home(request):
     return HttpResponse('<h1>Kids Meetup Home</h1>')
+
+class EventList(ListView):
+    model = Event
+
