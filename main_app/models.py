b987afdb1179a2a2bcded8e72d5b53b17e1919fa
Student Workspace <student@Candices-MacBook-Air.local>
2019-03-15 09:45:58 -0500

    corrected event model and insert phone field to parent model

--- main_app/models.py
+++ main_app/models.py
@@ -5,11 +5,18 @@ from django.db import models
 class Parent(models.Model):
     name=models.CharField(max_length=100)
     address=models.CharField(max_length=100)
+    phone=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
 
 class Event(models.Model):
-    name:models.CharField(max_length=2)
-    address:models.CharField(max_length=100)
+    name=models.CharField(max_length=100)
+    address=models.CharField(max_length=100)
     parent=models.ForeignKey(Parent,on_delete=100)
+
+    def __str__(self):
+        return self.name
     
 class Child(models.Model):
     name=models.CharField(max_length=100)
@@ -17,5 +24,7 @@ class Child(models.Model):
     parent=models.ForeignKey(Parent,on_delete=models.CASCADE)
     event=models.ForeignKey(Event,on_delete=models.CASCADE)
 
+    def __str__(serlf):
+        return self.name
 
     
