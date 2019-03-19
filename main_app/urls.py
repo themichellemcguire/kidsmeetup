af5cdf229aa11acb91381fe5a3395bdcbdb363fe
Student Workspace <student@Candices-MacBook-Air.local>
2019-03-19 10:35:24 -0500

    adds changes

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('events/', views.EventList.as_view(),name='events_list'),
+    path('events/<int:pk'/,views.EventDetail)
     path('events/create', views.EventCreate.as_view(),name='events_create'),
 
     path('accounts/', include('django.contrib.auth.urls')),
