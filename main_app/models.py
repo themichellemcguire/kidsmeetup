c36753ce655220662d70fb24f88c9f6e9e3c7eb2
owner <themichellemcguire@gmail.com>
2019-03-20 15:24:14 -0500

    commit to pull

--- main_app/models.py
+++ main_app/models.py
@@ -23,7 +23,6 @@ class Parent(models.Model):
     name=models.CharField(max_length=100)
     address=models.CharField(max_length=100)
     phone=models.CharField(max_length=10)
- 
     user=models.OneToOneField(User,on_delete=models.CASCADE)
 
 @receiver(post_save, sender=User)
@@ -34,7 +33,6 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.parent.save()
-
 
     def __str__(self):
         return self.name
